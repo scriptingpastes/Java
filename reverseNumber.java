697e4e8bcb3df0cd2ee2cddd2ddeb0c13898e2d1
scriptingpastes <103373930+scriptingpastes@users.noreply.github.com>
2024-01-29 21:42:10 -0600

    Create reverseNumber.java

---
+++ reverseNumber.java
@@ -0,0 +1,15 @@
+import.java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner keyBoard = new Scanner(System.in);
+    int input = keyBoard.nextInt();
+    int res = 0;
+    while (input != 0){
+      res += input % 10;
+      res = res * 10;
+      input = input / 10;
+    }
+    res = res / 10;
+    System.out.println(res);
+  }
+}
